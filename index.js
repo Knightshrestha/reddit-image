6dc383e3c1a9facc1ce75fea2d3f80acce815820
Knightshrestha <copyrekxh759@gmail.com>
2022-04-09 01:37:16 +0545

    added feature

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-import axios from 'axios';
+const axios = require('axios');
 
-let url = 'https://api.reddit.com/r/AnimeWallpaperSFW/hot?limit=10'
+async function ImageExtractor(subreddit, limit) {
+	let url = 'https://api.reddit.com/r/AnimeWallpaperSFW/hot?limit=10';
 
-const response = await axios
-  .get(url)
-  .then((res) => res.data)
-	// .catch((err) => console.log(err));
+	const response = await axios.get(url).then((res) => res.data);
+
+	const { data } = response;
+	const { children } = data;
+
+	children.map((child) => {
+		const { data: info } = child;
+		const { link_flair_text: type, title, subreddit, url } = info;
+		console.log('type', type + ' ' + url + ' ' + title);
+	});
+
+}
+
+ImageExtractor()
 
 
-const { data } = response
-const { children } = data 
-
-children.map((child) => {
-	const { data: info } = child;
-	const { link_flair_text: type, title, subreddit, url } = info;
-	console.log('type', type + " " + url + " " + title);
-});
-
-
-
-// const {link_flair_text: type , subreddit, url } = info
-
+module.exports = {
+	ImageExtractor
+};
